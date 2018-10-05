82fad4bcebc02cf4cf30f616ebf8d09e13071b51
matt2tech <mlipsey@basecampcodingacademy.org>
2018-10-05 12:32:34 -0500

    myFunction activates the nav tabs but thats it

--- navtabs.js
+++ navtabs.js
@@ -2,10 +2,9 @@ function myFunction() {
         var nav = document.querySelectorAll('.nav-item');
         var tab = document.querySelectorAll('.tab-pane');
         nav.forEach(function(x) {
-            var num = nav.indexOf(x);
-            var btn = x.querySelector(x[num])
-            btn.addEventListener('click', function() {
-                x.classList.toggle('show', 'active')
+            x.addEventListener('click', function() {
+                x.classList.toggle('show')
+                x.classList.toggle('active'
             })
         })
     }
