ed632a840254c35677dcdbcda56c031b7a4b43c9
matt2tech <mlipsey@basecampcodingacademy.org>
2018-10-05 14:59:18 -0500

    myFunction now shows the active nav tab and the tab associated with it

--- navtabs.js
+++ navtabs.js
@@ -1,14 +1,17 @@
 function myFunction() {
-        var nav = document.querySelectorAll('.nav-item');
-        var tab = document.querySelectorAll('.tab-pane');
-        nav.forEach(function(x) {
-            x.addEventListener('click', function() {
-                for (var i = 0; i < nav.length; i++) {
-                   nav[i].classList.remove('active') 
-                }
-                x.classList.toggle('active');
-            })
-        })
-    }
+    var nav = document.querySelectorAll(".nav-item");
+    var tab = document.querySelectorAll(".tab-pane");
+    nav.forEach(function(x) {
+        x.addEventListener("click", function() {
+            for (var i = 0; i < nav.length; i++) {
+                nav[i].classList.remove("active");
+                tab[i].classList.remove("active");
+                x.classList.add("active");
+            }
+            num = Array.from(nav).indexOf(x);
+            tab[num].classList.add("active");
+        });
+    });
+}
 
-myFunction()
+myFunction();
